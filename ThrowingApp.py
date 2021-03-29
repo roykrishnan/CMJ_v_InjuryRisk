5d47562c3dd153d70514d3703e91572118854c68
roykrishnan <60521440+roykrishnan@users.noreply.github.com>
2021-03-29 18:37:07 -0400

    Add files via upload

---
+++ ThrowingApp.py
@@ -0,0 +1,10 @@
+"""
+An Application designed by Rohit (Roy) Krishnan
+Designed to help in gym as well as remote athletes create a competitive throwing environment conducive to quicker
+growth. The code behind this project uses velocity as a filtering variable in order to allow those who throw at
+similar speeds to be partnered with each other, thereby fostering healthy competition and maximum growth.
+"""
+import kivy
+from kivy.app import App
+
+class MyApp (App):
